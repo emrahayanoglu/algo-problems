4670a69e92dfe6bf87801c5023f4dc6fedb407f8
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2018-04-05 11:12:30 +0000

    Find Duplicate O(n) solution introduced

--- cpp_algo/find_duplicate.cpp
+++ cpp_algo/find_duplicate.cpp
@@ -14,10 +14,25 @@ int findDuplicate(std::vector<int>& nums){
     }
 }
 
+int findDuplicate(int nums[], int length){
+    
+    for(int i = 0; i < length; i++){
+        if(nums[abs(nums[i]) - 1] < 0){
+            return nums[i];
+        }
+        nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+    }
+    
+    return -1;
+}
+
+
+
 int main(){
     std::vector<int> nums = {2,1,2};
-    
+    int arrayNums[] = {2,1,2};
     std::cout << findDuplicate(nums) << std::endl;
+    std::cout << findDuplicate(arrayNums, 3) << std::endl;
     
     return 0;
 }
